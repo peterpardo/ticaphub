d51967b552060807ab6d41f09e90a8548f18d353
Migs <migspajarillo19@gmail.com>
2022-04-19 12:15:06 +0800

    Fixed User Capstone's Group

--- resources/views/user-accounts/view-group.blade.php
+++ resources/views/user-accounts/view-group.blade.php
@@ -3,9 +3,9 @@
 
     <a href="/users/groups" class="bg-red-500 hover:bg-red-600 rounded text-white px-2 py-1">Back</a>
 
-    <div class="w-1/2 mx-auto mt-3 rounded shadow-md px-3 py-2">
+    <div class="w-1/2 mx-auto bg-white rounded-xl shadow-xl px-4 py-2">
         <h1 class="text-center text-2xl font-semibold my-1">{{ $group->name }}</h1>
-        @if($group->userGroups->count() == 0) 
+        @if($group->userGroups->count() == 0)
             <div class="bg-gray-100 rounded text-center py-5">No members</div>
         @else
             <p class="text-lg">Members:</p>
