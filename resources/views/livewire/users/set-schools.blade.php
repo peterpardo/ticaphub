41ca5c6ee462dbf3326f855af5bec6f70ec4e180
Peter Carl Pardo <peterpardo123@gmail.com>
2022-06-18 17:53:34 +0800

    Created table component

--- resources/views/livewire/users/set-schools.blade.php
+++ resources/views/livewire/users/set-schools.blade.php
@@ -43,91 +43,83 @@
     <hr class="my-3 h-1 rounded-lg bg-gray-100">
 
     {{-- Specializations --}}
-    <div class="antialiased font-sans">
-        <div class="mx-auto">
-            <div class="flex flex-col mb-3 space-y-2 sm:flex-row sm:items-center sm:space-y-0">
-                <h2 class="inline-block text-2xl font-bold mr-2">Specializations</h2>
-                <span class="inline-block px-2 py-.5 bg-gray-100 rounded text-sm text-gray-500">Add specializations for each school</span>
-            </div>
-
-            {{-- Specialization Form --}}
-            <form wire:submit.prevent="addSpecialization" class="space-y-3 w-full max-w-xs">
-                <div class="flex flex-col space-y-1">
-                    <label for="selectedSchool" class="text-sm tracking-wide">Select School</label>
-                    <select wire:model="selectedSchool" id="selectedSchool" class="w-full py-2 px-3 rounded border border-gray-500 text-sm">
-                        @foreach($schools->where('is_involved', 1) as $school)
-                            <option value="{{ $school->id }}">{{ $school->name }}</option>
-                        @endforeach
-                    </select>
-                    {{-- Error message --}}
-                    @error('selectedSchool')
-                        <span class="text-sm tracking-wide bg-red-100 text-red-500 rounded-lg py-1 px-3">{{ $message }}</span>
-                    @enderror
-                </div>
-                <div class="flex flex-col space-y-1">
-                    <label for="name" class="text-sm tracking-wide">Specialization Name (Complete Name)</label>
-                    <input type="text" wire:model.defer="name" id="name" class="w-full py-2 px-3 rounded border border-gray-500 text-sm" placeholder="Ex: Web and Mobile Application" autocomplete="off">
-                    {{-- Error message --}}
-                    @error('name')
-                        <span class="text-sm tracking-wide bg-red-100 text-red-500 rounded-lg py-1 px-3">{{ $message }}</span>
-                    @enderror
-                </div>
-                <button type="submit" class="p-2 text-sm text-white rounded-lg bg-green-600 hover:bg-green-500">Add Specialization</button>
-            </form>
-
-            {{-- Specialization Table --}}
-            <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
-                <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
-                    <table class="min-w-full leading-normal">
-                        <thead>
-                            <tr>
-                                <th
-                                    class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                                    School
-                                </th>
-                                <th
-                                    class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                                    Specialization
-                                </th>
-                                <th
-                                    class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                                    Actions
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @forelse ($specializations as $specialization)
-                                <tr>
-                                    {{-- School Name --}}
-                                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                        <p class="text-gray-900 whitespace-no-wrap">{{ $specialization->school->name }}</p>
-                                    </td>
-                                    {{-- Specialization Name --}}
-                                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                        <p class="text-gray-900 whitespace-no-wrap">{{ $specialization->name }}</p>
-                                    </td>
-                                    {{-- Actions --}}
-                                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                        <button type="button" wire:click="openModal('delete', {{ $specialization->id }})"class="text-white rounded p-2 text-xs tracking-wide bg-red-600 hover:bg-red-500">
-                                            <i class="fa-solid fa-trash"></i>
-                                            <span class="hidden tracking-wide md:inline-block">Delete</span>
-                                        </button>
-                                    </td>
-                                </tr>
-                            @empty
-                                <tr>
-                                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                        <p class="text-gray-900 whitespace-no-wrap">No specializations found</p>
-                                    </td>
-                                </tr>
-                            @endforelse
-                        </tbody>
-                    </table>
-                </div>
-                {{ $specializations->links() }}
-            </div>
-        </div>
+    <div class="flex flex-col mb-3 space-y-2 sm:flex-row sm:items-center sm:space-y-0">
+        <h2 class="inline-block text-2xl font-bold mr-2">Specializations</h2>
+        <span class="inline-block px-2 py-.5 bg-gray-100 rounded text-sm text-gray-500">Add specializations for each school</span>
     </div>
+
+    {{-- Specialization Form --}}
+    <form wire:submit.prevent="addSpecialization" class="space-y-3 w-full max-w-xs">
+        <div class="flex flex-col space-y-1">
+            <label for="selectedSchool" class="text-sm tracking-wide">Select School</label>
+            <select wire:model="selectedSchool" id="selectedSchool" class="w-full py-2 px-3 rounded border border-gray-500 text-sm">
+                @foreach($schools->where('is_involved', 1) as $school)
+                    <option value="{{ $school->id }}">{{ $school->name }}</option>
+                @endforeach
+            </select>
+            {{-- Error message --}}
+            @error('selectedSchool')
+                <span class="text-sm tracking-wide bg-red-100 text-red-500 rounded-lg py-1 px-3">{{ $message }}</span>
+            @enderror
+        </div>
+        <div class="flex flex-col space-y-1">
+            <label for="name" class="text-sm tracking-wide">Specialization Name (Complete Name)</label>
+            <input type="text" wire:model.defer="name" id="name" class="w-full py-2 px-3 rounded border border-gray-500 text-sm" placeholder="Ex: Web and Mobile Application" autocomplete="off">
+            {{-- Error message --}}
+            @error('name')
+                <span class="text-sm tracking-wide bg-red-100 text-red-500 rounded-lg py-1 px-3">{{ $message }}</span>
+            @enderror
+        </div>
+        <button type="submit" class="p-2 text-sm text-white rounded-lg bg-green-600 hover:bg-green-500">Add Specialization</button>
+    </form>
+
+    {{-- Specialization Table --}}
+    <x-table>
+        <x-slot name="heading">
+            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                School
+            </th>
+            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                Specialization
+            </th>
+            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                Actions
+            </th>
+        </x-slot>
+
+        <x-slot name="body">
+            @forelse ($specializations as $specialization)
+                <tr>
+                    {{-- School Name --}}
+                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap">{{ $specialization->school->name }}</p>
+                    </td>
+                    {{-- Specialization Name --}}
+                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap">{{ $specialization->name }}</p>
+                    </td>
+                    {{-- Actions --}}
+                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <button type="button" wire:click="openModal('delete', {{ $specialization->id }})"class="text-white rounded p-2 text-xs tracking-wide bg-red-600 hover:bg-red-500">
+                            <i class="fa-solid fa-trash"></i>
+                            <span class="hidden tracking-wide md:inline-block">Delete</span>
+                        </button>
+                    </td>
+                </tr>
+            @empty
+                <tr>
+                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap">No specializations found</p>
+                    </td>
+                </tr>
+            @endforelse
+        </x-slot>
+
+        <x-slot name="links">
+            {{ $specializations->links() }}
+        </x-slot>
+    </x-table>
+
 
     {{-- Delete modal --}}
     {{-- NOTE: This modal can only be used inside a livewire component --}}
