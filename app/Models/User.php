a7ca23f9bd59483a23222fffa088ec546a3d3b34
Peter Carl Pardo <peterpardo123@gmail.com>
2022-06-28 21:35:19 +0800

    create accessor for User model names

--- app/Models/User.php
+++ app/Models/User.php
@@ -118,6 +118,18 @@ class User extends Authenticatable
         });
     }
 
+    public function getFirstNameAttribute($value) {
+        return ucfirst($value);
+    }
+
+    public function getMiddleNameAttribute($value) {
+        return ucfirst($value);
+    }
+
+    public function getLastNameAttribute($value) {
+        return ucfirst($value);
+    }
+
     public function getFullnameAttribute() {
         return $this->first_name . ' ' . $this->middle_name . ' ' . $this->last_name;
     }
