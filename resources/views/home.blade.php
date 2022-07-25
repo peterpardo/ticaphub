c92329d376dcc851026c6d7603aa52564d56be9d
Peter Carl Pardo <peterpardo123@gmail.com>
2022-07-25 09:03:40 +0800

    Fixed code in home.blade.php and guest.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,143 +7,144 @@
 </head>
 
 <x-guest-layout>
-    <body class="overflow-x-auto animate-pulse">
-    @if($sliders->count() == 0)
-    <div class="hidden"></div>
-    @else
-<section>
-    <div class="w-full mx-auto overflow-hidden">
-        <div id="slider" class="swiper-container w-full mx-auto">
-            <div class="swiper-wrapper">
-                @foreach($sliders as $key => $slider)
-                <div class="swiper-slide bg-cover bg-center h-full text-white py-24 px-10 object-fill" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url({{asset($slider->image)}}); background-repeat: no-repeat; background-size: cover;">
-                    <div class="container mx-auto px-6 md:px-20 py-6">
-                        <div class="w-full md:w-1/2">
-                            <div class="mt-24">
-                                <h3 class="text-5xl font-bold">{{ $slider->title }}</h3>
-                                <p class="text-2xl pt-10 leading-none">{{ $slider->description}}</p>
+    <div class="overflow-x-auto">
+        {{-- Slider --}}
+        @if($sliders->count() == 0)
+            <div class="hidden"></div>
+            @else
+            <section>
+                <div class="w-full mx-auto overflow-hidden">
+                    <div id="slider" class="swiper-container w-full mx-auto">
+                        <div class="swiper-wrapper">
+                            @foreach($sliders as $key => $slider)
+                            <div class="swiper-slide bg-cover bg-center h-full text-white py-24 px-10 object-fill" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url({{asset($slider->image)}}); background-repeat: no-repeat; background-size: cover;">
+                                <div class="container mx-auto px-6 md:px-20 py-6">
+                                    <div class="w-full md:w-1/2">
+                                        <div class="mt-24">
+                                            <h3 class="text-5xl font-bold">{{ $slider->title }}</h3>
+                                            <p class="text-2xl pt-10 leading-none">{{ $slider->description}}</p>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
+                            @endforeach
+
                         </div>
+                        <div class="hidden md:flex swiper-button-prev bg-transparent w-10 h-w-10 text-xs rounded-full text-red-600"></div>
+                        <div class="hidden md:flex swiper-button-next bg-transparent w-10 h-w-10 text-xs rounded-full text-red-600"></div>
+                        <div class="swiper-pagination"></div>
+                </div>
+            </section>
+        @endif
+
+        {{-- MAIN CONTENT --}}
+        {{-- @foreach($streams as $key => $stream)
+            <main data-aos="fade-up" class="container mx-auto flex flex-row items-center p-5 w-auto h-screen justify-evenly mt-40 sm:mt-0">
+                <div class="grid grid-col md:grid-cols-2">
+                    <div class="flex flex-row md:justify-start justify-center">
+                        <div class="flex flex-col w-full md:w-3/4 object-cover h-auto justify-items-start rounded-lg overflow-hidden"
+                            style="min-heigth:320px">
+                <div class="">
+                    <div class="w-full h-full object-cover object-center block rounded">
+                        {!! $stream->stream_link !!}
                     </div>
                 </div>
-                @endforeach
+                        </div>
 
+                        </div>
+
+                        <div class="flex flex-row items-center">
+                            <div class="flex flex-col">
+                                <h1 class="capitalize text-4xl font-extrabold mb-3">{{ $stream->title }}</h1>
+                                <p class="w-auto text-lg text-gray-500 text-justify">{{ $stream->description }}</p>
+                                <div class="flex items-center my-6 cursor-pointer">
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+            </main>
+        @endforeach --}}
+
+        <section class="text-gray-600 body-font">
+            <div data-aos="fade-down" class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+                <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                    <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">TICaP Hub: An Event Management System for FEU Tech's Technology Innovation on Capstone Projects
+                        <br class="hidden lg:inline-block">
+                    </h1>
+                    <p class="mb-8 leading-relaxed text-justify">
+                        FEU Tech’s College of Computer Studies hosts an annual event known as Technology Innovation on Capstone Project (TICaP) which exhibits the different capstone projects of each specialization. This event allows the students to view the different projects of the FEU Tech PBL students and recognizes the group with the best capstone project and other special awards. For the event to be successful, an efficient organization and management must be executed.
+                    </p>
+                </div>
+
+                <div class="sm:w-1/2 w-full">
+                    <img class="object-cover object-center rounded-lg" src="{{ url('assets/ticaphub.png') }}">
+                </div>
             </div>
-            <div class="hidden md:flex swiper-button-prev bg-transparent w-10 h-w-10 text-xs rounded-full text-red-600"></div>
-            <div class="hidden md:flex swiper-button-next bg-transparent w-10 h-w-10 text-xs rounded-full text-red-600"></div>
-            <div class="swiper-pagination"></div>
-    </div>
-</section>
-@endif
-{{-- MAIN CONTENT --}}
-@foreach($streams as $key => $stream)
-	<main data-aos="fade-up" class="container mx-auto flex flex-row items-center p-5 w-auto h-screen justify-evenly mt-40 sm:mt-0">
-		<div class="grid grid-col md:grid-cols-2">
-			<div class="flex flex-row md:justify-start justify-center">
-				<div class="flex flex-col w-full md:w-3/4 object-cover h-auto justify-items-start rounded-lg overflow-hidden"
-					style="min-heigth:320px">
-          <div class="">
-            <div class="w-full h-full object-cover object-center block rounded">
-                {!! $stream->stream_link !!}
-            </div>
-          </div>
-				</div>
-
-				</div>
-
-				<div class="flex flex-row items-center">
-					<div class="flex flex-col">
-						<h1 class="capitalize text-4xl font-extrabold mb-3">{{ $stream->title }}</h1>
-						<p class="w-auto text-lg text-gray-500 text-justify">{{ $stream->description }}</p>
-						<div class="flex items-center my-6 cursor-pointer">
-						</div>
-					</div>
-				</div>
-
-
-			</div>
-	</main>
-    @endforeach
-
-    <section class="text-gray-600 body-font">
-      <div data-aos="fade-down" class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-        <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">TICaP Hub: An Event Management System for FEU Tech's Technology Innovation on Capstone Projects
-            <br class="hidden lg:inline-block">
-          </h1>
-          <p class="mb-8 leading-relaxed text-justify">
-            FEU Tech’s College of Computer Studies hosts an annual event known as Technology Innovation on Capstone Project (TICaP) which exhibits the different capstone projects of each specialization. This event allows the students to view the different projects of the FEU Tech PBL students and recognizes the group with the best capstone project and other special awards. For the event to be successful, an efficient organization and management must be executed.
-        </p>
-        </div>
-        <div class="sm:w-1/2 w-full">
-          <img class="object-cover object-center rounded-lg" src="{{ url('assets/ticaphub.png') }}">
-        </div>
-      </div>
-    </section>
+        </section>
 
         <section data-aos="fade-up-right" class="container p-6 mx-auto bg-transparent dark:bg-gray-800 mb-10 rounded">
             @if($brands->count() == 0)
-            <div class="hidden"></div>
+                <div class="hidden"></div>
             @else
-          <h2 class="text-xl font-medium text-gray-800 capitalize dark:text-white md:text-2xl">TICaP Events</h2>
+                <h2 class="text-xl font-medium text-gray-800 capitalize dark:text-white md:text-2xl">TICaP Events</h2>
 
-          <div class="flex items-center justify-center">
-              <div class="grid gap-8 mt-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-                @foreach($brands as $brand)
-                <a href="{{ $brand->image }}" data-featherlight="image">
-                  <div class="w-full max-w-xs text-center">
-                      <img class="object-cover object-center w-full h-48 mx-auto rounded-lg" src="{{ $brand->image }}" alt="avatar"/>
-                  </div>
-                </a>
-                @endforeach
-              </div>
-          </div>
-      </section>
-      @endif
+                <div class="flex items-center justify-center">
+                    <div class="grid gap-8 mt-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                        @foreach($brands as $brand)
+                            <a href="{{ $brand->image }}" data-featherlight="image">
+                                <div class="w-full max-w-xs text-center">
+                                    <img class="object-cover object-center w-full h-48 mx-auto rounded-lg" src="{{ $brand->image }}" alt="avatar"/>
+                                </div>
+                            </a>
+                        @endforeach
+                    </div>
+                </div>
+            @endif
+        </section>
 
-      <section data-aos="fade-down-right" class="text-gray-600 body-font">
-        <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-          <img class="w-32 h-32  object-cover object-center rounded animate-bounce" alt="hero" src="{{ url('assets/cyberace.png') }}">
-          <div class="text-center lg:w-2/3 w-full">
-            <p class="leading-relaxed">Coded by:</p>
-            <h1 class="title-font sm:text-2xl text-3xl mb-4 font-medium text-gray-900">CYBER ACE</h1>
-          </div>
-        </div>
-      </section>
+        <section data-aos="fade-down-right" class="text-gray-600 body-font">
+            <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+                <img class="w-32 h-32  object-cover object-center rounded animate-bounce" alt="hero" src="{{ url('assets/cyberace.png') }}">
+                <div class="text-center lg:w-2/3 w-full">
+                    <p class="leading-relaxed">Coded by:</p>
+                    <h1 class="title-font sm:text-2xl text-3xl mb-4 font-medium text-gray-900">CYBER ACE</h1>
+                </div>
+            </div>
+        </section>
 
-      <script src="//code.jquery.com/jquery-latest.js"></script>
-      <script src="//cdn.jsdelivr.net/npm/featherlight@1.7.14/release/featherlight.min.js" type="text/javascript" charset="utf-8"></script>
-    </body>
+        @push('scripts')
+            <script src="//code.jquery.com/jquery-latest.js"></script>
+            <script src="//cdn.jsdelivr.net/npm/featherlight@1.7.14/release/featherlight.min.js" type="text/javascript" charset="utf-8"></script>
+            <script>
+                var mySwiper = new Swiper ('.swiper-container', {
+                    // Optional parameters
+                    direction: 'horizontal',
+                    loop: true,
+
+                    // If we need pagination
+                    pagination: {
+                        el: '.swiper-pagination',
+                    },
+
+                    // Navigation arrows
+                    navigation: {
+                        nextEl: '.swiper-button-next',
+                        prevEl: '.swiper-button-prev',
+                    },
+
+                    // And if we need scrollbar
+                    scrollbar: {
+                        el: '.swiper-scrollbar',
+                    },
+                    autoplay: {
+                        delay: 4000,
+                    },
+                    loop: true,
+                });
+            </script>
+        @endpush
+    </div>
 </x-guest-layout>
-<style>
-    .swiper-pagination-bullet-active {
-  background-color: rgba(220, 38, 38, var(--tw-text-opacity));
-}
-</style>
-<script>
-    var mySwiper = new Swiper ('.swiper-container', {
-        // Optional parameters
-        direction: 'horizontal',
-        loop: true,
 
-        // If we need pagination
-        pagination: {
-            el: '.swiper-pagination',
-        },
-
-        // Navigation arrows
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-
-        // And if we need scrollbar
-        scrollbar: {
-            el: '.swiper-scrollbar',
-        },
-        autoplay: {
-            delay: 4000,
-        },
-        loop: true,
-    });
-</script>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,136 +1,133 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>{{ config('app.name', 'Laravel') }}</title>
-        <link rel="icon" href="{{ url('assets/ticap-logo.png') }}"/>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" href="{{ url('assets/ticap-logo.png') }}"/>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    {{-- Custom CSS --}}
+    <style>
+        html,
+        body {
+            font-family: "Rubik", sans-serif;
+        }
 
-        <!-- Feather Light -->
-        <link href="//cdn.jsdelivr.net/npm/featherlight@1.7.14/release/featherlight.min.css" type="text/css" rel="stylesheet"/>
+        .swiper-pagination-bullet-active {
+            background-color: rgba(220, 38, 38, var(--tw-text-opacity));
+        }
 
-        <!-- AOS -->
-        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+        /* navigation
+        - show navigation always on the large screen devices with (min-width:1024)
+        */
+        @media (min-width: 1024px) {
+            .top-navbar {
+                display: inline-flex !important;
+            }
+        }
+    </style>
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-        <script src="{{ asset('js/homeburgermenu.js') }}" defer></script>
-
-          <!-- Feather Light Scripts -->
-        <script src="//code.jquery.com/jquery-latest.js"></script>
-        <script src="//cdn.jsdelivr.net/npm/featherlight@1.7.14/release/featherlight.min.js" type="text/javascript" charset="utf-8"></script>
-
-        {{-- SWIPER --}}
-        <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css">
-        <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
-        <script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
-        <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-        <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
-
-    </head>
+    {{-- CSS --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/3.0.1/iconfont/material-icons.min.css" integrity="sha256-x8PYmLKD83R9T/sYmJn1j3is/chhJdySyhet/JuHnfY=" crossorigin="anonymous" />
     <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700;900&display=swap" rel="stylesheet">
-<style>
-  html,
-body {
-  font-family: "Rubik", sans-serif;
-}
-/* navigation
- - show navigation always on the large screen devices with (min-width:1024)
-*/
-@media (min-width: 1024px) {
-  .top-navbar {
-    display: inline-flex !important;
-  }
-}
-</style>
 
-<nav class="flex items-center bg-red-800 p-3 flex-wrap">
-      <a href="{{ route ('home') }}" class="p-2 mr-4 inline-flex items-center">
-        <img class="w-7 h-7" src="{{ url('assets/ticap-logo.png') }}" alt="">
-        <span  class="text-xl text-white font-bold uppercase tracking-wide"
-          >TICaP HUB</span>
-      </a>
-      <button
-        class="text-white inline-flex p-3 hover:bg-white-900 rounded lg:hidden ml-auto hover:text-white outline-none nav-toggler"
-        data-target="#navigation">
-        <i class="material-icons">menu</i>
-      </button>
-      <div
-        class="hidden top-navbar w-full lg:inline-flex lg:flex-grow lg:w-auto"
-        id="navigation">
-        <div
-          class="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto">
-          <a
-            href="{{ route ('home') }}"
-            class="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white items-center justify-center hover:bg-red-900 hover:text-white">
-            <span>Home</span>
-          </a>
-          {{-- CHECK IF TICAP EXISTS --}}
-          @php
-              $admin = \App\Models\User::find(1);
-              $showExhibit = false;
-              if($admin->ticap_id) {
-                $ticap = \App\Models\Ticap::find($admin->ticap_id);
-                $showExhibit = $ticap->awards_is_set;
-              }
-          @endphp
-          @if($showExhibit)
-            <a
-            href="{{ route ('schools') }}"
-              class="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white items-center justify-center hover:bg-red-900 hover:text-white">
-              <span>Project Exhibit</span>
-            </a>
-          @endif
-          {{-- <a
-          href="{{ route('about-ticap') }}"
-          class="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white items-center justify-center hover:bg-red-900 hover:text-white">
-          <span>About Us</span>
-        </a> --}}
-		  @auth
-		  <a class="lg:inline-flex md:ml-5 bg-red-700 lg:w-auto w-full px-3 py-2 rounded text-white items-center justify-center hover:bg-red-400 hover:text-white" href="{{ route('dashboard') }}">Go to Dashboard</a>
-	  @else
-		  <a class="lg:inline-flex md:ml-5 bg-red-700 lg:w-auto w-full px-3 py-2 rounded text-white items-center justify-center hover:bg-red-400 hover:text-white" href="{{ route('login') }}">Sign in</a>
-	  @endauth
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <!-- Feather Light -->
+    <link href="//cdn.jsdelivr.net/npm/featherlight@1.7.14/release/featherlight.min.css" type="text/css" rel="stylesheet"/>
+
+    <!-- AOS -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/homeburgermenu.js') }}" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous" defer></script>
+
+    <!-- Feather Light Scripts -->
+    <script src="//code.jquery.com/jquery-latest.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/featherlight@1.7.14/release/featherlight.min.js" type="text/javascript" charset="utf-8"></script>
+
+    {{-- SWIPER --}}
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css">
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+    <script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+</head>
+
+<body class="bg-gray-100">
+    {{-- Navbar --}}
+    <nav class="flex items-center bg-red-800 p-3 flex-wrap">
+        <a href="{{ route ('home') }}" class="p-2 mr-4 inline-flex items-center">
+            <img class="w-7 h-7" src="{{ url('assets/ticap-logo.png') }}" alt="">
+            <span  class="text-xl text-white font-bold uppercase tracking-wide"
+            >TICAPHUB</span>
+        </a>
+
+        <button class="text-white inline-flex p-3 hover:bg-white-900 rounded lg:hidden ml-auto hover:text-white outline-none nav-toggler" data-target="#navigation">
+            <i class="material-icons">menu</i>
+        </button>
+
+        <div class="hidden top-navbar w-full lg:inline-flex lg:flex-grow lg:w-auto" id="navigation">
+            <div class="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto">
+                <a href="{{ route ('home') }}" class="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white items-center justify-center hover:bg-red-900 hover:text-white">
+                    <span>Home</span>
+                </a>
+
+                {{-- CHECK IF TICAP EXISTS --}}
+                @php
+                    $admin = \App\Models\User::find(1);
+                    $showExhibit = false;
+                    if($admin->ticap_id) {
+                        $ticap = \App\Models\Ticap::find($admin->ticap_id);
+                        $showExhibit = $ticap->awards_is_set;
+                    }
+                @endphp
+                @if($showExhibit)
+                    <a href="{{ route ('schools') }}" class="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white items-center justify-center hover:bg-red-900 hover:text-white">
+                        <span>Project Exhibit</span>
+                    </a>
+                @endif
+
+                @auth
+                    <a class="lg:inline-flex md:ml-5 bg-red-700 lg:w-auto w-full px-3 py-2 rounded text-white items-center justify-center hover:bg-red-400 hover:text-white" href="{{ route('dashboard') }}">Go to Dashboard</a>
+                @else
+                    <a class="lg:inline-flex md:ml-5 bg-red-700 lg:w-auto w-full px-3 py-2 rounded text-white items-center justify-center hover:bg-red-400 hover:text-white" href="{{ route('login') }}">Sign in</a>
+                @endauth
+            </div>
         </div>
-      </div>
     </nav>
 
+    {{-- Main Content --}}
+    <div class="font-sans text-gray-900 antialiased">
+        {{ $slot }}
+    </div>
 
+    {{-- Scripts --}}
+    @stack('scripts')
+    <script>
+        $(document).ready(function() {
+            $(".nav-toggler").each(function(_, navToggler) {
+                const target = $(navToggler).data("target");
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
-<script>
-  $(document).ready(function() {
-  $(".nav-toggler").each(function(_, navToggler) {
-    var target = $(navToggler).data("target");
-    $(navToggler).on("click", function() {
-      $(target).animate({
-        height: "toggle"
-      });
-    });
-  });
-});
-</script>
-    <body class="bg-gray-100">
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
-        </div>
-    </body>
-      <script>
-        AOS.init();
-      </script>
-        {{-- <footer class="text-gray-600 body-font text-center">
-          <div class="container px-5 py-8 mx-auto">
-            <p class="text-sm text-gray-500">© 2021 Cyber Ace</p>
-          </div>
-        </footer> --}}
+                $(navToggler).on("click", function() {
+                    $(target).animate({
+                        height: "toggle"
+                    });
+                });
+            });
+        });
+    </script>
+    <script>AOS.init();</script>
+</body>
+
 </html>
